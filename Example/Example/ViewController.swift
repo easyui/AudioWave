890cc00a496ecac9851d9698fc3d2d9d204f7eec
cactus <522941939@qq.com>
2016-08-08 15:01:55 +0800

    delete error code

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -18,10 +18,8 @@ class ViewController: UIViewController ,AudioWaveDataSource{
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-    @IBOutlet weak var audioWaveFromXib: AudioWave!
     
-    self.audioWaveFromXib?.dataSource = self
-    self.audioWaveFromXib?.start()
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
